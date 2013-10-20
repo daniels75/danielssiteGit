88b35ef6d9c89d77f80c472dd8c7aaa1dc10b16e
Daniel Sadowski <daniel.sadowski75@gmail.com>
2013-10-20 22:31:02 +0200

    modif1

--- src/test/java/org/daniels/projects/site/pages/UserEditTest.java
+++ src/test/java/org/daniels/projects/site/pages/UserEditTest.java
@@ -13,79 +13,79 @@ import static org.junit.Assert.*;
 
 public class UserEditTest extends BasePageTestCase {
 
-//    @Test
-//    public void testCancel() throws Exception {
-//        doc = tester.renderPage("admin/UserList");
-//        Element table = doc.getElementById("userList");
-//        List<Node> rows = table.find("tbody").getChildren();
-//        String username = "";
-//
-//        username = ((Element) rows.get(0)).find("td/a").getChildMarkup().trim();
-//
-//        Element idLink = table.getElementById("user-" + username);
-//        doc = tester.clickLink(idLink);
-//
-//
-//        Element cancelButton = doc.getElementById("cancel");
-//
-//        doc = tester.clickLink(cancelButton);
-//
-//        // force locale=en (APF-1324)
-//        ResourceBundle rb = ResourceBundle.getBundle(MESSAGES, new Locale("en"));
-//
-//        assertTrue(doc.toString().contains(rb.getString("userList.title")));
-//    }
+    @Test
+    public void testCancel() throws Exception {
+        doc = tester.renderPage("admin/UserList");
+        Element table = doc.getElementById("userList");
+        List<Node> rows = table.find("tbody").getChildren();
+        String username = "";
 
-//    @Test
-//    public void testSave() throws Exception {
-//
-//        doc = tester.renderPage("admin/UserList");
-//        Element addLink = doc.getElementById("add");
-//
-//        doc = tester.clickLink(addLink);
-//
-//        Element form = doc.getElementById("form");
-//        assertNotNull(form);
-//
-//        fieldValues.put("username", "tapestry");
-//        fieldValues.put("password", "isfun");
-//        fieldValues.put("confirmPassword", "isfun");
-//        fieldValues.put("passwordHint", "funstuff");
-//        fieldValues.put("firstName", "Tapestry");
-//        fieldValues.put("lastName", "5");
-//        fieldValues.put("email", "tapestry@appfuse.org");
-//        fieldValues.put("website", "http://tapestry.apache.org");
-//        fieldValues.put("city", "Portland");
-//        fieldValues.put("state", "OR");
-//        fieldValues.put("postalCode", "97303");
-//        fieldValues.put("country", "US");
-//
-//        // start SMTP Server
-//        Wiser wiser = new Wiser();
-//        wiser.setPort(getSmtpPort());
-//        wiser.start();
-//
-//        doc = tester.submitForm(form, fieldValues);
-//
-//        Element errors = doc.getElementById("errorMessages");
-//
-//        if (errors != null) {
-//            log.error(errors);
-//        }
-//
-//        assertNull(doc.getElementById("errorMessages"));
-//
-//        // verify an account information e-mail was sent
-//        assertEquals(1, wiser.getMessages().size());
-//        wiser.stop();
-//
-//        //Element successMessages = doc.getElementById("successMessages");
-//        //assertNotNull(successMessages);
-//        //assertTrue(successMessages.toString().contains("added successfully"));
-//        assertTrue(doc.toString().contains("added successfully"));
-//        Element table = doc.getElementById("userList");
-//        assertTrue(table.toString().contains("tapestry"));
-//    }
+        username = ((Element) rows.get(0)).find("td/a").getChildMarkup().trim();
+
+        Element idLink = table.getElementById("user-" + username);
+        doc = tester.clickLink(idLink);
+
+
+        Element cancelButton = doc.getElementById("cancel");
+
+        doc = tester.clickLink(cancelButton);
+
+        // force locale=en (APF-1324)
+        ResourceBundle rb = ResourceBundle.getBundle(MESSAGES, new Locale("en"));
+
+        assertTrue(doc.toString().contains(rb.getString("userList.title")));
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+        doc = tester.renderPage("admin/UserList");
+        Element addLink = doc.getElementById("add");
+
+        doc = tester.clickLink(addLink);
+
+        Element form = doc.getElementById("form");
+        assertNotNull(form);
+
+        fieldValues.put("username", "tapestry");
+        fieldValues.put("password", "isfun");
+        fieldValues.put("confirmPassword", "isfun");
+        fieldValues.put("passwordHint", "funstuff");
+        fieldValues.put("firstName", "Tapestry");
+        fieldValues.put("lastName", "5");
+        fieldValues.put("email", "tapestry@appfuse.org");
+        fieldValues.put("website", "http://tapestry.apache.org");
+        fieldValues.put("city", "Portland");
+        fieldValues.put("state", "OR");
+        fieldValues.put("postalCode", "97303");
+        fieldValues.put("country", "US");
+
+        // start SMTP Server
+        Wiser wiser = new Wiser();
+        wiser.setPort(getSmtpPort());
+        wiser.start();
+
+        doc = tester.submitForm(form, fieldValues);
+
+        Element errors = doc.getElementById("errorMessages");
+
+        if (errors != null) {
+            log.error(errors);
+        }
+
+        assertNull(doc.getElementById("errorMessages"));
+
+        // verify an account information e-mail was sent
+        assertEquals(1, wiser.getMessages().size());
+        wiser.stop();
+
+        //Element successMessages = doc.getElementById("successMessages");
+        //assertNotNull(successMessages);
+        //assertTrue(successMessages.toString().contains("added successfully"));
+        assertTrue(doc.toString().contains("added successfully"));
+        Element table = doc.getElementById("userList");
+        assertTrue(table.toString().contains("tapestry"));
+    }
 
     @Test
     public void testRemove() throws Exception {
